c36ce0300bcd94fb50e2ac7536e95a181e5dd912
Raoul Andalis <raoulandalis1@yahoo.com>
2024-06-17 15:47:51 -0400

    created welcome component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@
 
 import React from 'react'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import Landing from './pages/Landing';
+import Landing from './pages/Landing'
+import Welcome from './pages/Welcome'
 
 
 function App() {
   return (
-    <main>
+    <main className='w-full h-screen relative bg-black'>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Landing/>}/>
+          <Route path='/welcome' element={<Welcome/>}/>
         </Routes>
       </BrowserRouter>
     </main>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Cave from '../assets/cave.png'
+import { NavLink } from 'react-router-dom'
 
 const Landing = () => {
 
@@ -8,7 +9,10 @@ const Landing = () => {
       <section className='flex justify-center py-48 text-white'>
         <div className='flex flex-col items-center text-8xl italic'>
           <h1>Untitled</h1>
-          <h1>Escape Room</h1>
+          <h1 className='pb-16'>Escape Room</h1>
+          <NavLink to='/welcome'>
+          <button type="button" class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-2xl px-20 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">Start</button>
+          </NavLink>
         </div>
       </section>
     </main>
