64eabd2daf87c365d5a6552051fc12d72e6e8ea9
Raoul Andalis <raoulandalis1@yahoo.com>
2024-06-17 15:26:57 -0400

    started landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Cave from '../assets/cave.png'
 
 const Landing = () => {
+
   return (
-    <div className='text-3xl'>Landing Page</div>
+    <main className='w-full h-screen relative bg-cover bg-center' style={{ backgroundImage: `url(${Cave})` }}>
+      <section className='flex justify-center py-48 text-white'>
+        <div className='flex flex-col items-center text-8xl italic'>
+          <h1>Untitled</h1>
+          <h1>Escape Room</h1>
+        </div>
+      </section>
+    </main>
   )
 }
 
